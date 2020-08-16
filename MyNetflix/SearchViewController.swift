843f7c152f3f9b94bd5e09f1cf82da80c19f4771
sssssungs <32057999+sssssungs@users.noreply.github.com>
2020-08-16 15:46:00 +0900

    added searchbar component and delegated to view controller

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // 키보드 없애기
+        dismissKeyboard()
+        
+        // 검색어 있는지 확인
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else {
+            return
+        }
+        
+        print("search term ====> \(searchTerm)")
+        
+        // 네트워킹
+    }
 }
